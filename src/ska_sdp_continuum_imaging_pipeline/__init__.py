b7ad6ce0645b25e80d94d2b3e48c554f7146085d
Vincent Morello <vmorello@gmail.com>
2024-06-20 13:40:28 +0100

    HIP-936: Add dummy code and tests

---
+++ src/ska_sdp_continuum_imaging_pipeline/__init__.py
@@ -0,0 +1,5 @@
+def return_one() -> int:
+    """
+    Just a dummy function to test the initial repo setup.
+    """
+    return 1
---
+++ tests/dummy_test.py
@@ -0,0 +1,8 @@
+from ska_sdp_continuum_imaging_pipeline import return_one
+
+
+def test_return_one():
+    """
+    Just a dummy test for initial repo setup.
+    """
+    assert return_one() == 1
