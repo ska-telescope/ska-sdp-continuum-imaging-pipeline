0f0ff7f01a8ae7e7d39868a78b4339d8938ccad0
Vincent Morello <vmorello@gmail.com>
2024-06-26 15:49:47 +0100

    Add simple CLI app

---
+++ src/skip/apps/pipeline_app.py
@@ -0,0 +1,64 @@
+import argparse
+import sys
+from pathlib import Path
+
+import numpy as np
+
+from skip import MeasurementSet, __version__, invert_measurement_set
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """
+    Create the CLI parser for the app.
+    """
+    parser = argparse.ArgumentParser(
+        description=("Launch the SKA continuum imaging pipeline"),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument(
+        "measurement_set", type=Path, help="Path to MeasurementSet v2"
+    )
+    parser.add_argument(
+        "output_image",
+        type=Path,
+        help="Path to output image, which is saved as a numpy array",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-pixels",
+        type=int,
+        required=True,
+        help="Number of pixels across the image",
+    )
+    parser.add_argument(
+        "-p",
+        "--pixel-size",
+        type=float,
+        required=True,
+        help="Pixel size in arcseconds at the image centre",
+    )
+    return parser
+
+
+def run_program(cli_args: list[str]) -> None:
+    """
+    Run the app. This is the function called by the tests.
+    """
+    args = get_parser().parse_args(cli_args)
+    mset = MeasurementSet(args.measurement_set)
+    img = invert_measurement_set(
+        mset, num_pixels=args.num_pixels, pixel_size_asec=args.pixel_size
+    )
+    np.save(args.output_image.with_suffix(".npy"), img)
+
+
+def main() -> None:
+    """
+    Entry point for the pipeline app.
+    """
+    run_program(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ tests/test_pipeline_app.py
@@ -0,0 +1,31 @@
+import tempfile
+from pathlib import Path
+
+import numpy as np
+
+from skip import MeasurementSet
+from skip.apps.pipeline_app import run_program
+
+
+def test_pipeline_app(measurement_set: MeasurementSet):
+    """
+    Run the pipeline app on the test measurement set.
+    """
+    num_pixels = 1600
+    pixel_size_asec = 4.0
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        image_path = Path(tempdir) / "image.npy"
+        cli_args = [
+            "-n",
+            str(num_pixels),
+            "-p",
+            str(pixel_size_asec),
+            str(measurement_set.path),
+            str(image_path),
+        ]
+        run_program(cli_args)
+
+        assert image_path.is_file()
+        data = np.load(image_path)
+        assert data.shape == (num_pixels, num_pixels)
