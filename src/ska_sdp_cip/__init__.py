c38a21b53e572cb943f1fd3a587c0265f1185ca9
Vincent Morello <vmorello@gmail.com>
2024-07-04 13:11:13 +0100

    Add dask distribution of invert

--- src/ska_sdp_cip/__init__.py
+++ src/ska_sdp_cip/__init__.py
@@ -1,5 +1,10 @@
 from ._version import __version__
 from .measurement_set import MeasurementSetReader
-from .stages import invert_measurement_set
+from .stages import dask_invert_measurement_set, invert_measurement_set
 
-__all__ = ["__version__", "MeasurementSetReader", "invert_measurement_set"]
+__all__ = [
+    "__version__",
+    "MeasurementSetReader",
+    "dask_invert_measurement_set",
+    "invert_measurement_set",
+]
