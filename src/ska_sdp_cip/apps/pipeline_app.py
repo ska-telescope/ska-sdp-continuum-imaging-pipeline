46065f3ae3abe4804655e8f9178b0fb466190807
Vincent Morello <vmorello@gmail.com>
2024-07-05 10:44:04 +0100

    HIP-961: Add dask distribution options to CLI app

--- src/ska_sdp_cip/apps/pipeline_app.py
+++ src/ska_sdp_cip/apps/pipeline_app.py
@@ -3,10 +3,12 @@ import sys
 from pathlib import Path
 
 import numpy as np
+from dask.distributed import Client
 
 from ska_sdp_cip import (
     MeasurementSetReader,
     __version__,
+    dask_invert_measurement_set,
     invert_measurement_set,
 )
 
@@ -28,19 +30,49 @@ def get_parser() -> argparse.ArgumentParser:
         type=Path,
         help="Path to output image, which is saved as a numpy array",
     )
-    parser.add_argument(
+
+    # Imaging options
+    imaging_group = parser.add_argument_group("imaging")
+    imaging_group.add_argument(
         "-n",
         "--num-pixels",
         type=int,
         required=True,
         help="Number of pixels across the image",
     )
-    parser.add_argument(
+    imaging_group.add_argument(
         "-p",
         "--pixel-size",
         type=float,
         required=True,
         help="Pixel size in arcseconds at the image centre",
+    )
+
+    # Dask options
+    dask_group = parser.add_argument_group("dask distribution")
+    dask_group.add_argument(
+        "-d",
+        "--dask-scheduler",
+        type=str,
+        default=None,
+        help="Optional address of a dask scheduler to use for distribution",
+    )
+    dask_group.add_argument(
+        "-rc",
+        "--row-chunks",
+        type=int,
+        default=1,
+        help="Number of row chunks to use for distribution",
+    )
+    dask_group.add_argument(
+        "-fc",
+        "--freq-chunks",
+        type=int,
+        default=None,
+        help=(
+            "Number of frequency chunks to use for distribution. "
+            "If None, set this to the number of dask workers."
+        ),
     )
     return parser
 
@@ -51,9 +83,22 @@ def run_program(cli_args: list[str]) -> None:
     """
     args = get_parser().parse_args(cli_args)
     mset = MeasurementSetReader(args.measurement_set)
-    img = invert_measurement_set(
-        mset, num_pixels=args.num_pixels, pixel_size_asec=args.pixel_size
-    )
+
+    if args.dask_scheduler is None:
+        img = invert_measurement_set(
+            mset, num_pixels=args.num_pixels, pixel_size_asec=args.pixel_size
+        )
+    else:
+        client = Client(args.dask_scheduler)
+        img = dask_invert_measurement_set(
+            mset,
+            client,
+            num_pixels=args.num_pixels,
+            pixel_size_asec=args.pixel_size,
+            row_chunks=args.row_chunks,
+            freq_chunks=args.freq_chunks,
+        )
+
     np.save(args.output_image.with_suffix(".npy"), img)
 
 
--- tests/test_pipeline_app.py
+++ tests/test_pipeline_app.py
@@ -3,6 +3,7 @@ import tempfile
 from pathlib import Path
 
 import numpy as np
+from dask.distributed import Client
 
 from ska_sdp_cip import MeasurementSetReader
 from ska_sdp_cip.apps.pipeline_app import run_program
@@ -32,6 +33,42 @@ def test_pipeline_app(ms_reader: MeasurementSetReader):
             str(ms_reader.path),
             str(image_path),
         ]
+        run_program(cli_args)
+
+        assert image_path.is_file()
+        data = np.load(image_path)
+        assert data.shape == (num_pixels, num_pixels)
+
+
+def test_pipeline_app_with_dask(
+    ms_reader: MeasurementSetReader, dask_client: Client
+):
+    """
+    Run the pipeline app on the test measurement set using dask distribution.
+    """
+    num_pixels = 2048
+    pixel_size_asec = 5.0
+
+    cli_args_dict = {
+        "-n": num_pixels,
+        "-p": pixel_size_asec,
+        "--dask-scheduler": str(dask_client.scheduler.address),
+        "--row-chunks": 2,
+        "--freq-chunks": 2,
+    }
+
+    # Optional arguments
+    cli_args = []
+    for key, val in cli_args_dict.items():
+        cli_args.append(key)
+        cli_args.append(str(val))
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        image_path = Path(tempdir) / "image.npy"
+
+        # Positional arguments
+        cli_args.extend([str(ms_reader.path), str(image_path)])
+        print(cli_args)
         run_program(cli_args)
 
         assert image_path.is_file()
