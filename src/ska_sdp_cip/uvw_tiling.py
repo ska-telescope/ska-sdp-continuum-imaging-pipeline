4a64e434b3062e8238f28b6491f5b89b6bf16d06
Vincent Morello <vmorello@gmail.com>
2024-08-08 19:03:18 +0100

    Refactor

--- src/ska_sdp_cip/uvw_tiling.py
+++ src/ska_sdp_cip/uvw_tiling.py
@@ -1,30 +1,109 @@
 from collections import defaultdict
+from typing import NamedTuple
 
 import numpy as np
 from numpy.typing import NDArray
 
-TileIndex = tuple[int, int, int]
+TileCoords = tuple[int, int, int]
 """
-Integer tuple (iu, iv, iw) containing the coordinates of a tile in units
-of the tile size. Indices can be negative. (0, 0, 0) corresponds to the central
-tile, which is itself centered on u = v = w = 0.0.
+Tile index of the form (iu, iv, iw)
 """
 
-RowSliceIndex = tuple[int, int, int]
-"""
-Integer tuple (row_index, channel_start, channel_stop).
-"""
 
-SPEED_OF_LIGHT = 299792458.0
+class TileChunk(NamedTuple):
+    """
+    Aggregation of integer tile coordinates (iu, iv, iw) and of a chunk index.
+    The chunk index exists because some tiles contain many visibilities and
+    need to be split into smaller data pieces.
+    """
+
+    coords: TileCoords
+    ichunk: int
+
+
+class RowSlice(NamedTuple):
+    """
+    Specifies a slice of a particular visibility row along the frequency axis.
+    """
+
+    irow: int
+    chan_start: int
+    chan_stop: int
+
+
+TilingPlan = dict[TileChunk, list[RowSlice]]
+
+DEFAULT_MAX_VIS_PER_CHUNK = 1_000_000
+
+
+def create_uvw_tile_mapping(
+    uvw: NDArray, tile_size: tuple[float, float, float], channel_freqs: NDArray
+) -> dict[TileCoords, list[RowSlice]]:
+    """
+    Bin the UVW coordinates of visibilities by UVW tile.
+    """
+    SPEED_OF_LIGHT = 299792458.0
+    wavelength_inv = channel_freqs.reshape(-1, 1) / SPEED_OF_LIGHT
+    tile_size = np.asarray(tile_size)
+
+    tile_mapping: dict[TileCoords, list[RowSlice]] = defaultdict(list)
+
+    for irow, row_uvw in enumerate(uvw):
+        # The +0.5 takes into account the fact that the tile with index
+        # (0, 0, 0) is *centered* on the origin.
+        tile_indices = np.floor(
+            wavelength_inv * (row_uvw / tile_size) + 0.5
+        ).astype(int)
+
+        # Row slices correspond to sequences of constant tile coordinates
+        # in the array above.
+        subarrays = _find_all_constant_tile_index_subarrays(tile_indices)
+        for start_ch, stop_ch, tile_index in subarrays:
+            tile_mapping[tile_index].append(RowSlice(irow, start_ch, stop_ch))
+
+    return tile_mapping
+
+
+def split_uvw_tile_mapping(
+    tile_mapping: dict[TileCoords, list[RowSlice]],
+    max_vis_per_chunk: int = DEFAULT_MAX_VIS_PER_CHUNK,
+) -> TilingPlan:
+    """
+    Further split the tiles that contain more visibility samples than the
+    specified maximum. Returns a new mapping TileChunk -> list of RowSlices.
+    """
+    tiling_plan: dict[TileChunk, list[RowSlice]] = defaultdict(list)
+
+    for tile_index, row_slices in tile_mapping.items():
+        tile_chunk = TileChunk(tile_index, 0)
+        chunk_population = 0
+
+        for row_slice in row_slices:
+            num_vis = row_slice.chan_stop - row_slice.chan_start
+
+            # Create new chunk if chunk population would exceed limit
+            if (chunk_population + num_vis) >= max_vis_per_chunk:
+                tile_chunk = TileChunk(
+                    tile_chunk.coords, tile_chunk.ichunk + 1
+                )
+                chunk_population = 0
+
+            tiling_plan[tile_chunk].append(row_slice)
+            chunk_population += num_vis
+
+    return dict(tiling_plan)
 
 
 def create_uvw_tiling_plan(
     uvw: NDArray,
     tile_size: tuple[float, float, float],
     channel_freqs: NDArray,
-) -> dict[TileIndex, list[RowSliceIndex]]:
+    max_vis_per_chunk: int = DEFAULT_MAX_VIS_PER_CHUNK,
+) -> TilingPlan:
     """
-    Bin the UVW coordinates of a set of visibilities by UVW tile.
+    Bin the UVW coordinates of visibilities by UVW tile. Tiles whose total
+    number of visibilities would exceed the given limit are further split
+    into smaller chunks.
 
     Args:
         uvw: A 2D numpy array of shape (num_rows, 3) containing UVW
@@ -32,32 +111,23 @@ def create_uvw_tiling_plan(
         tile_size: A tuple of three floats representing the size of the tiles
             in the U, V, and W directions.
         channel_freqs: A 1D numpy array containing the channel frequencies.
+        max_vis_per_chunk: Maximum number of visibility samples that can be
+            contained into a data chunk.
 
     Returns:
-        A dictionary where keys are integer tile indices of the form
-        (iu, iv, iw) and values are lists of tuples of the form
-        (row_index, channel_start, channel_stop). `row_index` refers to the
-        input `uvw` array.
+        tiling_plan: A dictionary where keys are `TilingChunk` namedtuples,
+            and the values are lists of `RowSlice` namedtuples.
+            `TilingChunk` is the aggregation of a 3-tuple of tile coordinates
+            and of a chunk index. `RowSlice` carries a row index, start channel
+            index and stop channel index.
     """
-    wavelength_inv = channel_freqs.reshape(-1, 1) / SPEED_OF_LIGHT
-    tile_size = np.asarray(tile_size)
-    plan = defaultdict(list)
-
-    for irow, row_uvw in enumerate(uvw):
-        # The +0.5 takes into account the fact that the tile with index
-        # (0, 0, 0) is *centered* on the origin.
-        tile_indices = np.floor(wavelength_inv * (row_uvw / tile_size) + 0.5)
-        subarrays = _find_all_constant_tile_index_subarrays(tile_indices)
-        for start_ch, stop_ch, tile_index in subarrays:
-            index = tuple(tile_index)
-            plan[index].append((irow, start_ch, stop_ch))
-
-    return dict(plan)
+    tile_mapping = create_uvw_tile_mapping(uvw, tile_size, channel_freqs)
+    return split_uvw_tile_mapping(tile_mapping, max_vis_per_chunk)
 
 
 def _find_all_constant_tile_index_subarrays(
     arr: NDArray, offset: int = 0
-) -> list[tuple[int, int, TileIndex]]:
+) -> list[tuple[int, int, TileCoords]]:
     """
     Find the parameters of all constant subarrays in given array of tile
     indices (iu, iv, iw) using a recursive binary search.
@@ -68,7 +138,7 @@ def _find_all_constant_tile_index_subarrays(
     """
     n = len(arr)
     if np.array_equal(arr[0], arr[-1]):
-        return [(offset, offset + n, arr[0])]
+        return [(offset, offset + n, tuple(arr[0]))]
 
     half = n // 2
     head = _find_all_constant_tile_index_subarrays(arr[:half], offset)
--- tests/test_uvw_tiling.py
+++ tests/test_uvw_tiling.py
@@ -12,6 +12,7 @@ def test_create_uvw_tiling_plan(ms_reader: MeasurementSetReader):
     uvw = ms_reader.uvw()
     num_rows = len(uvw)
     tile_size = (3_000.0, 3_000.0, 6_000.0)
+    max_vis_per_chunk = 1_000_000
 
     # Us the Parameters of the first 256 channels of MeerKAT L-Band
     freq_start = 856.0e6
@@ -20,13 +21,15 @@ def test_create_uvw_tiling_plan(ms_reader: MeasurementSetReader):
     freq_step = bandwidth / nchan
     channel_freqs = freq_start + freq_step * np.arange(nchan)
 
-    plan = create_uvw_tiling_plan(uvw, tile_size, channel_freqs)
+    plan = create_uvw_tiling_plan(
+        uvw, tile_size, channel_freqs, max_vis_per_chunk=max_vis_per_chunk
+    )
 
     # Assert that each visibility sample appears in one tile exactly
     num_tiles_hit = np.zeros(shape=(num_rows, nchan), dtype=int)
 
     for row_slice_list in plan.values():
-        for irow, start_ch, stop_ch in row_slice_list:
-            num_tiles_hit[irow, start_ch:stop_ch] += 1
+        for x in row_slice_list:
+            num_tiles_hit[x.irow, x.chan_start : x.chan_stop] += 1
 
-    assert (num_tiles_hit == 0).sum() == 0
+    assert (num_tiles_hit == 1).sum() == num_tiles_hit.size
