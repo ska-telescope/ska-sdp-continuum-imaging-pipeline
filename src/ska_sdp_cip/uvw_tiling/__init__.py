cf845d4855b53102e5e3068b97d8dd604069fddb
Vincent Morello <vmorello@gmail.com>
2024-08-09 11:52:10 +0100

    Expose function that splits tile mapping into chunks

--- src/ska_sdp_cip/uvw_tiling/__init__.py
+++ src/ska_sdp_cip/uvw_tiling/__init__.py
@@ -6,11 +6,13 @@ from .core import (
     TileMapping,
     create_uvw_tile_chunking_plan,
     create_uvw_tile_mapping,
+    split_uvw_tile_mapping_into_chunks,
 )
 
 __all__ = [
     "create_uvw_tile_mapping",
     "create_uvw_tile_chunking_plan",
+    "split_uvw_tile_mapping_into_chunks",
     "RowSlice",
     "TileCoords",
     "TileChunk",
